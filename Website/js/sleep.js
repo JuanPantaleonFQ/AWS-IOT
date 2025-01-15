6403fbae08a43b05bc8032252eccb8aef2f3ec99
pt712016 <56408526+pt712016@users.noreply.github.com>
2025-01-15 05:39:03 +0000

    added a debug console.log

--- Website/js/sleep.js
+++ Website/js/sleep.js
@@ -11,6 +11,7 @@ $.get('/check-sleep-status', function (data) {
 $(document).ready(function () {
     // When the "Start Sleeping" button is clicked
     $('#sleepButton').on('click', function () {
+        console.log("button clicked")
         if ($(this).text() === "Start sleeping") {
             const startTime = new Date().toISOString();  // Capture the current time in ISO format
 
