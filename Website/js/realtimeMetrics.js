abebaffba9862e7f91358b1dee71670c9af4f71e
pt712016 <56408526+pt712016@users.noreply.github.com>
2025-01-15 08:31:52 +0000

    if this doesnt work its time to nuke

--- Website/js/realtimeMetrics.js
+++ Website/js/realtimeMetrics.js
@@ -132,22 +132,21 @@ function getRealTimeMetrics(datetime, period) {
 createChart();
 
 setInterval(function() {
-  getRealTimeMetrics("now", 5, function(data) {
-    // Extract the relevant data from the response
-    const newLabel = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); // Get current time (HH:MM)
-    const newTemperature = data.averageTemperature;
-    const newHumidity = data.averageHumidity;
-    const newLux = data.averageLux;
-    const newMotion = data.totalMotion;
+  var data = getRealTimeMetrics("now", 5)
+  // Extract the relevant data from the response
+  const newLabel = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); // Get current time (HH:MM)
+  const newTemperature = data.averageTemperature;
+  const newHumidity = data.averageHumidity;
+  const newLux = data.averageLux;
+  const newMotion = data.totalMotion;
 
-    // Add new data point to the chart
-    chartData.labels.push(newLabel);  // Add time label
-    chartData.datasets[0].data.push(newTemperature);  // Add temperature
-    chartData.datasets[1].data.push(newHumidity);  // Add humidity
-    chartData.datasets[2].data.push(newLux);  // Add light (lux)
-    chartData.datasets[3].data.push(newMotion);  // Add movement
+  // Add new data point to the chart
+  chartData.labels.push(newLabel);  // Add time label
+  chartData.datasets[0].data.push(newTemperature);  // Add temperature
+  chartData.datasets[1].data.push(newHumidity);  // Add humidity
+  chartData.datasets[2].data.push(newLux);  // Add light (lux)
+  chartData.datasets[3].data.push(newMotion);  // Add movement
 
     // Update the chart with the new data
     metricsChart.update();
-  });
 }, period * 60 * 1000);
